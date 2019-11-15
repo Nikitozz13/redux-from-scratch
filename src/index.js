518e71f8983cba2aacc0e402378d0a412c479d0b
Nikita Staroseltsev <n.staroseltsev@smartcat.pro>
2019-11-15 17:35:28 +0300

    Some changes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter.jsx';
 
+import './Counter.css';
+
 ReactDOM.render(<Counter/>, document.getElementById('root'));
